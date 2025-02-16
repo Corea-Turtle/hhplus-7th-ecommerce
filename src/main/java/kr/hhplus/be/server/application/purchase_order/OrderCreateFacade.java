d0a32d901cce7599e5809445c0c7ceb6c4e49b35
c_turtle <eastino@gmail.com>
2025-02-16 19:30:08 +0900

    [feat] 주문생성 파사드 추가

--- src/main/java/kr/hhplus/be/server/application/purchase_order/OrderCreateFacade.java
+++ src/main/java/kr/hhplus/be/server/application/purchase_order/OrderCreateFacade.java
@@ -40,18 +40,14 @@ public class OrderCreateFacade {
     private final PurchaseOrderService purchaseOrderService;
 
 
-/*    Long userId; //주문자ㅓ 아이디
-    List<Product> orderProducts; //주문 상품 리스트(상품id, 이름, 주문 수량 등을 담고 있음);
-    List<UserCoupon> usedCoupons;
-    long totalPrice;
-    String state*/
+
     public PurchaseOrderCreateResponse createOrder(PurchaseOrderCreateRequest request){
         //유저 아이디가 있는지 확인
         User user = userService.getExistUser(request.getUserId());
 
-        List<Product> products = new ArrayList<>();
+        List<ProductDTO> products = new ArrayList<>();
         List<UserCoupon> usedCoupons = new ArrayList<>();
-        usedCoupons.addAll(request.getUsedCoupons());
+        usedCoupons.addAll(request.getUsedCoupons().stream().toList());
 
         long totalPrice = 0;
         long afterCalPrice = 0;
@@ -59,31 +55,38 @@ public class OrderCreateFacade {
         int couponRate = 0;
         int couponDiscount = 0;
 
+        int zeroCnt = 0;
+
         //reqeust 상품 목록에 상품 재고 존재하는지 확인 후 상품 추가
-        for(Product product : request.getOrderProducts()){
+        for(ProductDTO product : request.getOrderProducts()){
+
             Product foundProduct = productService.getProductById(product.getId());
             //여기에 dblock을 걸어야하나
-            Stock stock = stockService.getStockByProductId(product.getId());
 
-            int productRequestQuantity = foundProduct.getOrderAmount();
+            int productRequestQuantity = product.getOrderAmount();
 
             int stockRemain = stockService.getStockRemainQuantity(product.getId());
 
-            try{
-                if(stockRemain >= productRequestQuantity){
-                    Product foundProductWithAmount = new Product(foundProduct.getId(), foundProduct.getName(), foundProduct.getPrice(), foundProduct.getOrderAmount());
-                    //상품을 리스트에 추가
-                    products.add(foundProductWithAmount);
-                    //상품 재고 감소
-                    stockService.decreaseStockByProductId(foundProductWithAmount.getId(),foundProductWithAmount.getOrderAmount());
-                    //총 상품 가격 합에 상품 가격 * 주문 개수 추가
-                    totalPrice += (long)foundProduct.getPrice() * stockRemain;
-                }else{
-                    throw new IllegalArgumentException("요청 상품 {상품 이름}의 재고가 부족하여 주문에 추가하지 못했습니다..");
-                }
-            }catch(Exception e){
-                //e.stackTrace();
+
+            if(stockRemain >= productRequestQuantity){
+                ProductDTO foundProductWithAmount = new ProductDTO(foundProduct.getId(), foundProduct.getName(), foundProduct.getPrice(), product.getOrderAmount());
+                //상품을 리스트에 추가
+                products.add(foundProductWithAmount);
+                //상품 재고 감소
+                stockService.decreaseStockByProductId(foundProductWithAmount.getId(),foundProductWithAmount.getOrderAmount());
+                //총 상품 가격 합에 상품 가격 * 주문 개수 추가
+                totalPrice += (long)foundProduct.getPrice() * stockRemain;
+            }else{
+
+                zeroCnt += 1;
+
+                //TODO 메시지 생성 "요청 상품 {상품 이름}의 재고가 부족하여 주문에 추가하지 못했습니다..");
             }
+
+        }
+
+        if(zeroCnt == request.getOrderProducts().size()){
+            throw new IllegalArgumentException("요청하신 모든 상품의 재고가 부족하여 주문 생성이 불가능합니다.");
         }
 
         //쿠폰 사용 여부
@@ -110,8 +113,10 @@ public class OrderCreateFacade {
         //최종 가격 생성(정률할인 먼저 한 후 정액 할인을 한다.)
         afterCalPrice = (totalPrice * (100-couponRate) / 100) - couponDiscount;
 
+        //TODO 유저 밸런스와 쿠폰 적용 후 가격을 비교후 exception처리 한다.
+
         //주문 생성(주문 완료) 상태로 주문 생성 응답 객체 반환
-        return new PurchaseOrderCreateResponse(request.getUserId(), products,usedCoupons,afterCalPrice);
+        return new PurchaseOrderCreateResponse(request.getUserId(), products, usedCoupons,afterCalPrice);
     }
 
 }
